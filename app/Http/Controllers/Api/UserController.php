f0d7270338145a6534856557b4c4dfd227b33300
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2020-07-24 03:16:05 +0900

    もろもろ修正

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use User;
 
 class UserController extends Controller
 {
-    public function index(): Response
+    public function index(Request $request)
     {
         $result = User::getUsers();
-        return response()->success($result);
+        return response()->json($result);
     }
 }
